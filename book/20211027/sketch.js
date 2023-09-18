a92724a7b7f3f74b950e92b069586bf1f0bafccd
hysysk <hayashi.yosuke@gmail.com>
2023-09-18 22:47:30 +0900

    Add perpendicular line

--- book/20211027/sketch.js
+++ book/20211027/sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
 function draw() {
   background(255);
-  text("The line formula: ax + by + c = 0", 0, 10); 
+  text("The line formula: ax + by + c = 0", 0, 10);
   text("a = " + a + ", b = " + b + ", c = " + c, 0, 25);
 
   let root = 1.0 / (a * a + b * b);
@@ -20,14 +20,28 @@ function draw() {
   root = sqrt(root);
   let f = b * root;
   let g = -a * root;
-  
-  line(x0, y0, x0 + f * 1000, y0 + g * 1000);
+  let x1 = x0 + f * 1000;
+  let y1 = y0 + g * 1000;
+
+  line(x0, y0, x1, y1);
 
   let xj = mouseX;
   let yj = mouseY;
   let absq = a * a + b * b;
   let sr = a * xj + b * yj + c;
   let r = sqrt(sr * sr / absq);
-  ellipse(xj, yj, 4, 4);
+  ellipse(xj, yj, 8, 8);
   text("The shortest distance of the point from the line: " + r.toFixed(2), 0, 40);
+
+  let val = a * xj + b * yj + c;
+  let angle = atan2(y1 - y0, x1 - x0);
+  if (val < 0) {
+    angle = angle + HALF_PI;
+  } else {
+    angle = angle - HALF_PI;
+  }
+
+  let x2 = xj + r * cos(angle);
+  let y2 = yj + r * sin(angle);
+  line(xj, yj, x2, y2);
 }
